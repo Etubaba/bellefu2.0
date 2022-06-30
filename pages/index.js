0dd459bca94bc4206a2bc06c8ee6d525bd40ec38
Onyedikachukwu <isiwuemma@gmail.com>
2022-06-30 12:43:10 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -20,10 +20,27 @@ export default function Home({ data }) {
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const search = useSelector((state) => state.bellefu?.searchFilter);
+  const [isLocationReady, setLocationReady] = useState(false);
+  const [currData, setCurrData] = useState([]);
 
   // if (data) {
   //   dispatch(fetchData(data));
   // }
+
+  useEffect(() => {
+    const getCurrData = async () => {
+      await axios.get(indexAPI)
+      .then((res) => {
+        setCurrData(res.data);
+        setLocationReady(true);
+      })
+      .catch(error => {
+        console.log(`Error fetching index data: ${error.message}`)
+      })
+    }
+
+    getCurrData();
+  }, []);
 
   useEffect(() => {
     dispatch(fetchData(data));
@@ -56,17 +73,17 @@ export default function Home({ data }) {
         <div className="max-w-[95%] lg:max-w-[90%] mx-auto mt-24">
           {/* second nav bar */}
           <HeaderSearch
-            dialet={data.defaultLanguage}
-            state={data.countryStates}
-            defaultCountry={data.defaultCountryName}
-            languages={data.languages}
-            countries={data.countries}
-            location={data.defaultCountry}
+            dialet={isLocationReady?currData.defaultLanguage:data.defaultLanguage}
+            state={isLocationReady?currData.countryStates:data.countryStates}
+            defaultCountry={isLocationReady?currData.defaultCountryName:data.defaultCountryName}
+            languages={isLocationReady?currData.languages:data.languages}
+            countries={isLocationReady?currData.countries:data.countries}
+            location={isLocationReady?currData.defaultCountry:data.defaultCountry}
           />
 
           {search === "" && (
             <div className="block  md:hidden lg:hidden mt-3">
-              <Slider slider={data.slider} />
+              <Slider slider={isLocationReady?currData.slider:data.slider} />
             </div>
           )}
 
@@ -79,7 +96,7 @@ export default function Home({ data }) {
           <div className="flex flex-col lg:flex-row">
             {/* category side bar */}
             <div className=" hidden lg:inline w-[20%] h-auto rounded-md mr-3">
-              <CategorySideBar categories={data.categories} />
+              <CategorySideBar categories={isLocationReady? currData.categories:data.categories} />
             </div>
             {search === "" ? (
               <div className=" h-auto lg:hidden my-4 rounded-sm">
@@ -88,18 +105,18 @@ export default function Home({ data }) {
                     Search by categories
                   </h3>
 
-                  <MobileCategoryBar categories={data.categories} />
+                  <MobileCategoryBar categories={isLocationReady?currData.categories:data.categories} />
                 </div>
               </div>
             ) : null}
             {/* list of products & slider */}
             <div className="flex-1">
               <Body
-                location={data.defaultCountry}
-                currency={data.defaultCurrency}
-                currencyCode={data.defaultCurrencyCode}
-                productsData={data.products.data}
-                slider={data.slider}
+                location={isLocationReady?currData.defaultCountry:data.defaultCountry}
+                currency={isLocationReady?currData.defaultCurrency:data.defaultCurrency}
+                currencyCode={isLocationReady?currData.defaultCurrencyCode:data.defaultCurrencyCode}
+                productsData={isLocationReady?currData.products.data:data.products.data}
+                slider={isLocationReady?currData.slider:data.slider}
               />
             </div>
           </div>
