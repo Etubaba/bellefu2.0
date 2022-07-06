c5be9b6887457fc7214f5cd4bfac9d59a66d4a4a
dom000 <godknowsegiboy00@gmail.com>
2022-07-06 16:42:08 +0100

    new inputs

--- components/postAdsComponent/SubCatInput.js
+++ components/postAdsComponent/SubCatInput.js
@@ -1,55 +1,63 @@
-import * as React from 'react';
-import SelectUnstyled, { selectUnstyledClasses } from '@mui/base/SelectUnstyled';
-import OptionUnstyled, { optionUnstyledClasses } from '@mui/base/OptionUnstyled';
-import PopperUnstyled from '@mui/base/PopperUnstyled';
+import * as React from "react";
+import SelectUnstyled, {
+  selectUnstyledClasses,
+} from "@mui/base/SelectUnstyled";
+import OptionUnstyled, {
+  optionUnstyledClasses,
+} from "@mui/base/OptionUnstyled";
+import PopperUnstyled from "@mui/base/PopperUnstyled";
 import { handleSubcatUpdate } from "../../features/bellefuSlice";
 import { useSelector, useDispatch } from "react-redux";
-import { styled } from '@mui/system';
+import { styled } from "@mui/system";
 import { useState } from "react";
+import {MdOutlineKeyboardArrowDown,
+MdKeyboardArrowRight,
+} from "react-icons/md";
+
 const blue = {
-  100: '#DAECFF',
-  200: '#99CCF3',
-  400: '#3399FF',
-  500: '#007FFF',
-  600: '#0072E5',
+  100: "#DAECFF",
+  200: "#99CCF3",
+  400: "#3399FF",
+  500: "#007FFF",
+  600: "#0072E5",
   900: "E8F0FE",
 };
 
 const grey = {
-  100: '#E7EBF0',
-  200: '#E0E3E7',
-  300: '#CDD2D7',
-  400: '#B2BAC2',
-  500: '#A0AAB4',
-  600: '#6F7E8C',
-  700: '#3E5060',
-  800: '#2D3843',
-  900: '#1A2027',
+  100: "#E7EBF0",
+  200: "#E0E3E7",
+  300: "#CDD2D7",
+  400: "#B2BAC2",
+  500: "#A0AAB4",
+  600: "#6F7E8C",
+  700: "#3E5060",
+  800: "#2D3843",
+  900: "#1A2027",
 };
 
-const StyledButton = styled('button')(
+const StyledButton = styled("button")(
   ({ theme }) => `
   font-family: IBM Plex Sans, sans-serif;
   font-size: 0.875rem;
   box-sizing: border-box;
   min-height: calc(1.5em + 17px);
   min-width: 100%;
-  background: ${theme.palette.mode === 'dark' ? grey[900] : "#ffffff"};
-  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#ffffff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[800] : grey[300]};
   border-radius: 0.50em;
   margin-top: 0.29em;
   padding: 5px;
   text-align: left;
   line-height: 1.5;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
 
   &:hover {
-    background: ${theme.palette.mode === 'dark' ? '' : grey[100]};
-    border-color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
+    background: ${theme.palette.mode === "dark" ? "" : grey[100]};
+    border-color: ${theme.palette.mode === "dark" ? grey[700] : grey[400]};
   }
 
   &.${selectUnstyledClasses.focusVisible} {
-    outline: 3px solid ${theme.palette.mode === 'dark' ? blue[900] : blue[900]};
+    outline: 3px solid ${theme.palette.mode === "dark" ? blue[900] : blue[900]};
   }
 
   &.${selectUnstyledClasses.expanded} {
@@ -62,10 +70,10 @@ const StyledButton = styled('button')(
     content: '▾';
     float: right;
   }
-  `,
+  `
 );
 
-const StyledListbox = styled('ul')(
+const StyledListbox = styled("ul")(
   ({ theme }) => `
   font-family: IBM Plex Sans, sans-serif;
   font-size: 0.875rem;
@@ -74,15 +82,15 @@ const StyledListbox = styled('ul')(
   margin: 10px 0;
   min-width: 26vw;
   height: 36vh;
-  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[800] : grey[300]};
   border-radius: 0.50em;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
   overflow: auto;
   outline: 0px;
   z-index:100;
   overflow-y:scroll;
-  `,
+  `
 );
 
 const StyledOption = styled(OptionUnstyled)(
@@ -97,29 +105,29 @@ const StyledOption = styled(OptionUnstyled)(
   }
 
   &.${optionUnstyledClasses.selected} {
-    background-color: ${theme.palette.mode === 'dark' ? blue[900] : blue[900]};
-    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    background-color: ${theme.palette.mode === "dark" ? blue[900] : blue[900]};
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
   }
 
   &.${optionUnstyledClasses.highlighted} {
-    background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
-    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    background-color: ${theme.palette.mode === "dark" ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
   }
 
   &.${optionUnstyledClasses.highlighted}.${optionUnstyledClasses.selected} {
-    background-color: ${theme.palette.mode === 'dark' ? blue[900] : blue[900]};
-    color: ${theme.palette.mode === 'dark' ? blue[900] : blue[900]};
+    background-color: ${theme.palette.mode === "dark" ? blue[900] : blue[900]};
+    color: ${theme.palette.mode === "dark" ? blue[900] : blue[900]};
   }
 
   &.${optionUnstyledClasses.disabled} {
-    color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
+    color: ${theme.palette.mode === "dark" ? grey[700] : grey[400]};
   }
 
   &:hover:not(.${optionUnstyledClasses.disabled}) {
-    background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
-    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    background-color: ${theme.palette.mode === "dark" ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
   }
-  `,
+  `
 );
 
 const StyledPopper = styled(PopperUnstyled)`
@@ -138,31 +146,91 @@ const CustomSelect = React.forwardRef(function CustomSelect(props, ref) {
 });
 
 export default function UnstyledSelectSimple2({ subCategory, checker }) {
+  const [open1, setOpen1] = useState(false);
+  const [inputdata, setInputData] = useState(null);
+  const [searchdata, setSearchData] = useState("");
 
-  const dispatch = useDispatch()
-
-
+  const dispatch = useDispatch();
 
   const handleThings = (counts) => {
-    dispatch(handleSubcatUpdate(counts.subCatId))
+    dispatch(handleSubcatUpdate(counts.subCatId));
     //  setSubcat(counts) ;
-
-
-  }
+  };
 
   return (
-    <CustomSelect disabled={checker === "" ? true : false} >
-      {subCategory?.map((counts, index) => (
-        <span onClick={() => handleThings(counts)}>
-          <StyledOption
-            key={index}
-            value={counts.subCatName}
-          >
-            {counts.subCatName}
-          </StyledOption>
-        </span>
-      ))}
+    // <CustomSelect disabled={checker === "" ? true : false} >
+    //   {subCategory?.map((counts, index) => (
+    //     <span onClick={() => handleThings(counts)}>
+    //       <StyledOption
+    //         key={index}
+    //         value={counts.subCatName}
+    //       >
+    //         {counts.subCatName}
+    //       </StyledOption>
+    //     </span>
+    //   ))}
 
-    </CustomSelect>
+    // </CustomSelect>
+
+    <div>
+      <div className="w-full">
+        <div
+       
+          onClick={() =>  {checker === "" ?  null :setOpen1(!open1)}}
+          className="flex items-center mb-2 hover:bg-crystamolBackground p-1 md:p-2 lg:p-1.5 rounded-md border bg-gray-200 mt-4 relative"
+        >
+          <div className="flex items-center flex-1 space-x-3 cursor-pointer select-none">
+            <h5 className="text-bellefuBlack1 font-medium whitespace-nowrap">
+              {inputdata ? inputdata : "Select Category"}
+            </h5>
+          </div>
+          {!open1 ? (
+            <div onClick={() => setOpen1(!open1)}>
+              <MdKeyboardArrowRight className="text-crystamolPink cursor-pointer text-2xl" />
+            </div>
+          ) : (
+            <div onClick={() => setOpen1(!open1)}>
+              <MdOutlineKeyboardArrowDown className="text-crystamolPink cursor-pointer text-2xl" />
+            </div>
+          )}
+        </div>
+        {open1 ? (
+          <div className="lg:w-[20%] bg-crystamolWhite rounded border z-[500] transition duration-300 ease-in absolute bg-white  p-2   ">
+            <input
+              onChange={(e) => setSearchData(e.target.value)}
+              type="text"
+              placeholder="Search"
+              className=" border mx-4 h-10 px-4 py-2 rounded-md text-sm text-gray-600 focus:border-bellefuOrange focus:outline-none focus:ring-1 focus:ring-black focus:ring-opacity-5"
+            />
+            <ul className="rounded  py-4">
+              {subCategory
+                .filter((count) => {
+                  if (searchdata === "") {
+                    return count;
+                  } else if (
+                    count.subCatName.toLowerCase().includes(searchdata?.toLowerCase())
+                  ) {
+                    return count;
+                  }
+                })
+                .map((counts, index) => (
+                  <li
+                    key={index}
+                    onClick={() => {
+                      handleThings(counts),
+                        setInputData(counts.subCatName),
+                        setOpen1(!open1),
+                        setSearchData("");
+                    }}
+                    className="px-4 py-2 hover:bg-gray-300 cursor-pointer hover:text-crystamolWhite flex space-x-5 items-center cursor-pointe rounded"
+                  >
+                    <pre> {counts.subCatName}</pre>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        ) : null}
+      </div>
+    </div>
   );
-};
+}
