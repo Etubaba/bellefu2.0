4f0cefb1ece55b01d925068abaddd333693299e5
Etumnu Prosper <johnikechukwu94@gmail.com>
2022-07-25 11:13:34 +0100

    shop update

--- constant.js
+++ constant.js
@@ -1,9 +1,9 @@
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
 
-export const imageBaseUrl = "https://bellefu.inmotionhub.xyz/";
-// export const imageBaseUrl =
-//   "https://phpstack-794034-2715115.cloudwaysapps.com/";
+// export const imageBaseUrl = "https://bellefu.inmotionhub.xyz/";
+export const imageBaseUrl =
+  "https://phpstack-794034-2715115.cloudwaysapps.com/";
 export const productImageUrl = `${imageBaseUrl}get/product/image/`;
 export const shopApi = `${imageBaseUrl}api/shop/`;
 
