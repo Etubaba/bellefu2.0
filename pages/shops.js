d3b50854a841436df3973a5f991454a68d10116d
Etumnu Prosper <johnikechukwu94@gmail.com>
2022-09-12 11:55:08 +0100

    update shops

--- pages/shops.js
+++ pages/shops.js
@@ -6,12 +6,33 @@ import {
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Loader, { shopApi } from "../constant";
+import Loader, { indexAPI, shopApi } from "../constant";
+import Slider from "../components/mainPageComponents/slider/Slider";
+import HeaderSearch from "../components/HeaderSearch";
+import MobileCategoryBar from "../components/MobileCategorybar/MobileCategoryBar";
+import CategorySideBar from "../components/CategorySideBar";
 
 const Shops = ({ shops }) => {
   const [page, setPage] = useState(1);
   const [totalPage, setTotalPage] = useState(shops.data.last_page);
   const [newShop, setNewShop] = useState([]);
+  const [currData, setCurrData] = useState([]);
+  const [search, setSearch] = useState([]);
+
+  useEffect(() => {
+    const getCurrData = async () => {
+      await axios
+        .get(indexAPI)
+        .then((res) => {
+          setCurrData(res.data);
+        })
+        .catch((error) => {
+          console.log(`Error fetching index data: ${error.message}`);
+        });
+    };
+
+    getCurrData();
+  }, []);
 
   useEffect(() => {
     if (page > 1) {
@@ -22,14 +43,13 @@ const Shops = ({ shops }) => {
     }
   }, [page]);
 
-  const shop = page === 1 ? shops.data.data : newShop;
+  const shop = newShop;
 
   const pageNumber = [];
   for (let i = 1; i <= totalPage; i++) {
     pageNumber.push(i);
   }
 
-  console.log("shop product", shops);
   return (
     <div>
       <Head>
@@ -45,9 +65,76 @@ const Shops = ({ shops }) => {
           href="https://www.linkpicture.com/q/bellefuApplogo.jpg"
         />
       </Head>
-      <div className="px-2">
-        <Body shops={shop} />
-      </div>
+      <main className="bg-bellefuBackground ">
+        <div className="max-w-[95%] lg:max-w-[90%] mx-auto mt-28">
+          {/* second nav bar */}
+          <HeaderSearch
+            dialet={currData.defaultLanguage}
+            state={currData.countryStates}
+            defaultCountry={currData.defaultCountryName}
+            languages={currData.languages}
+            countries={currData.countries}
+            location={currData.defaultCountry}
+          />
+
+          {search === "" && (
+            <div className="block  md:hidden lg:hidden mt-3">
+              <Slider slider={currData.slider} />
+            </div>
+          )}
+
+          {/* mobile header search */}
+          {/* <div className="md:hidden">
+            <MobileHeaderSearch defaultCountry={data.defaultCountry} />
+          </div> */}
+
+          {/* main body */}
+          <div className="flex flex-col lg:flex-row">
+            {/* category side bar */}
+            <div className=" hidden lg:inline w-[20%] h-auto rounded-md mr-3">
+              <CategorySideBar categories={currData.categories} />
+            </div>
+            {search === "" ? (
+              <div className=" h-auto lg:hidden my-4 rounded-sm">
+                <div>
+                  <h3 className=" block lg:hidden font-bold text-[1rem] sm:text-[1rem] m-5 lg:text-[1.2rem]">
+                    Search by categories
+                  </h3>
+
+                  <MobileCategoryBar categories={currData.categories} />
+                </div>
+              </div>
+            ) : null}
+            {/* list of products & slider */}
+            <div className="flex-1">
+              {/* <Body
+                location={
+                  isLocationReady
+                    ? currData.defaultCountry
+                    : data.defaultCountry
+                }
+                currency={
+                  isLocationReady
+                    ? currData.defaultCurrency
+                    : data.defaultCurrency
+                }
+                currencyCode={
+                  isLocationReady
+                    ? currData.defaultCurrencyCode
+                    : data.defaultCurrencyCode
+                }
+                productsData={
+                  isLocationReady ? currData.products.data : data.products.data
+                }
+                slider={isLocationReady ? currData.slider : data.slider}
+              /> */}
+              <div className="px-2">
+                <Body shops={shop} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
 
       {shop.length !== 0 && totalPage > 1 && (
         <div className="flex justify-center md:mb-0 mb-8 md:mt-10 mt-7 items-center w-full ">
